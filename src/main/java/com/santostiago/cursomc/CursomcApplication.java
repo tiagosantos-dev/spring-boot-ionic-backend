0936382c9b5e8e652ffa53fe7af78df1f2214f0b
Tiago Santos <tiagoltj@hotmail.com>
2020-06-10 13:53:34 -0300

    Criando operacao de instaciacao

--- src/main/java/com/santostiago/cursomc/CursomcApplication.java
+++ src/main/java/com/santostiago/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.santostiago.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.santostiago.cursomc.domain.Categoria;
+import com.santostiago.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+
+	
+	
+
 
 }
