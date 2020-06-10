44512db6acda4b51e31901ef6e3c5ce0456668ec
Tiago Santos <tiagoltj@hotmail.com>
2020-06-10 00:57:31 -0300

    criando repository e service para categoria

---
+++ src/main/java/com/santostiago/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.santostiago.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.santostiago.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
