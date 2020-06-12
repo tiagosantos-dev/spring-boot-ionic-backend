c30b621984b6d5153a03835a7c4263b024dc854e
Tiago Santos <tiagoltj@hotmail.com>
2020-06-11 22:03:51 -0300

    Estado e Cidade

--- src/main/java/com/santostiago/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/santostiago/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -15,6 +15,7 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
+		//Classe para receber a mensagem , status, e o timestamp do erro
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
